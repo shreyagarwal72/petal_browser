6a9642d487a95874aa7c8a211eb15e7c7beda542
Shrey Agarwal <sanjayvansu1973@gmail.com>
2026-09-06 18:29:36 +0530

    Fix: add missing size layout import in ContainedLoadingIndicator

--- app/src/main/java/com/petal/browser/compose/composable/ContainedLoadingIndicator.kt
+++ app/src/main/java/com/petal/browser/compose/composable/ContainedLoadingIndicator.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ContainedLoadingIndicator
