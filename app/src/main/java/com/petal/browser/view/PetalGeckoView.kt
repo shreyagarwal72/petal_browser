d2d475bb14ab7633781067d7d380ffe14ba30633
Shrey Agarwal <sanjayvansu1973@gmail.com>
2026-09-07 22:24:09 +0530

    Fix sign-in popup session ownership

--- app/src/main/java/com/petal/browser/view/PetalGeckoView.kt
+++ app/src/main/java/com/petal/browser/view/PetalGeckoView.kt
@@ -252,18 +252,20 @@ class PetalGeckoView @JvmOverloads constructor(
             }
 
             override fun onNewSession(session: GeckoSession, uri: String): GeckoResult<GeckoSession>? {
-                val act = getHostActivity()
-                if (act is com.petal.browser.activity.BrowserActivity) {
-                    val newView = PetalGeckoView(act)
-                    newView.isIncognito = isIncognito
-                    act.runOnUiThread {
-                        act.addAlbum(newView.title, uri, true, isIncognito)
+            override fun onNewSession(session: GeckoSession, uri: String): GeckoResult<GeckoSession>? {
+                val act = getHostActivity() as? com.petal.browser.activity.BrowserActivity ?: return null
+                val result = GeckoResult<GeckoSession>()
+                act.runOnUiThread {
+                    val popup = act.addAlbumForPopup("Sign-in", isIncognito)
+                    val popupSession = (popup as? PetalGeckoView)?.session
+                    if (popupSession != null) {
+                        result.complete(popupSession)
+                    } else {
+                        result.completeExceptionally(IllegalStateException("Could not create sign-in window"))
                     }
-                    return GeckoResult.fromValue(newView.session)
                 }
-                return null
+                return result
             }
-        }
 
         // Content Delegate
         session.contentDelegate = object : GeckoSession.ContentDelegate {
