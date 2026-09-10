5b79c1b80142d18e547ac2192e4dd8a6db6ac891
Shrey Agarwal <sanjayvansu1973@gmail.com>
2026-09-11 02:10:08 +0530

    Fix build: resolve missing animation imports and remove updater references in petal_browser

--- app/src/main/java/com/petal/browser/compose/tabs/PetalTabGridSwitcher.kt
+++ app/src/main/java/com/petal/browser/compose/tabs/PetalTabGridSwitcher.kt
@@ -7,10 +7,12 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.BorderStroke
